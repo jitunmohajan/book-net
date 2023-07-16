c9bf8949da31ca9b49efdd7d99d9d513e8d1c403
jitunmohajan <jitunmohajan@gmail.com>
2023-07-16 19:55:26 +0600

    [DONE] create private route

---
+++ src/routes/routes.tsx
@@ -0,0 +1,53 @@
+import { createBrowserRouter } from 'react-router-dom';
+import App from '@/App';
+import Login from '@/pages/Login';
+import NotFound from '@/pages/NotFound';
+import Home from '@/pages/Home';
+import Books from '@/pages/Books';
+import AddBook from '@/pages/AddBook';
+import Signup from '@/pages/Signup';
+import BookDetails from '@/pages/BookDetails';
+import PrivateRoute from './PrivateRoute';
+
+const routes = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: '/books',
+        element: <Books />,
+      },
+      {
+        path: '/book-details/:id',
+        element: <BookDetails />,
+      },
+      {
+        path: '/add-new-book',
+        element: (
+          <PrivateRoute>
+            <AddBook />,
+          </PrivateRoute>
+        ),
+      },
+    ],
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/signup',
+    element: <Signup />,
+  },
+  {
+    path: '*',
+    element: <NotFound />,
+  },
+]);
+
+export default routes;
